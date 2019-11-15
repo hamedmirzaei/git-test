94ccda54e5b1001656fbe1750b8d5cd9432c143a
Hamed Mirzaei <mirzaeihamed1368@gmail.com>
2019-11-15 17:02:53 +0330

    b-a added in subtract.js

--- subtract.js
+++ subtract.js
@@ -1,5 +1,5 @@
-// subtract.js
+// subtract.js [dev/commit1]
 // return subtraction of two numbers
-function subtract(a, b) {
-    return a - b;
+function subtract(a, b, bMINUSa) {
+    return (bMINUSa) ? b - a : a - b;
 };
